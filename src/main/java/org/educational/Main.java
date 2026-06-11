84341b33a05e422666add0a36aaefc9c5f32951e
Lucas Oliveira <lucasp.oliveira822@gmail.com>
2026-06-11 08:39:45 -0300

    feature: start create cli

---
+++ src/main/java/org/educational/Main.java
@@ -0,0 +1,12 @@
+package org.educational;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("tarefator-cli ");
+        input.next();
+    }
+}
