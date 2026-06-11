01f44e294c1671ff7ec89b3aea2994b0643394b9
Lucas Oliveira <lucasp.oliveira822@gmail.com>
2026-06-11 08:47:41 -0300

    feature: initial line completed

--- src/main/java/org/educational/Main.java
+++ src/main/java/org/educational/Main.java
@@ -5,8 +5,15 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        boolean exitCondition = false;
+        String command;
 
-        System.out.print("tarefator-cli ");
-        input.next();
+        while(!exitCondition) {
+            System.out.print("tarefator-cli ");
+            command = input.nextLine();
+            if (command.equals("exit")){
+                exitCondition = true;
+            }
+        }
     }
 }
