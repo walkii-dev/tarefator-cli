fa641b203eb4136ac03796ea21cce0d30141461b
Lucas Oliveira <lucasp.oliveira822@gmail.com>
2026-06-11 09:17:58 -0300

    feature: create task entity

---
+++ src/main/java/org/educational/Task.java
@@ -0,0 +1,12 @@
+package org.educational;
+
+import java.time.LocalDateTime;
+
+public class Task {
+    int id;
+    String description;
+    TaskStatus status;
+    LocalDateTime createdAt;
+    LocalDateTime updatedAt;
+
+}
---
+++ src/main/java/org/educational/TaskStatus.java
@@ -0,0 +1,8 @@
+package org.educational;
+
+public enum TaskStatus {
+    TODO,
+    IN_PROGRESS,
+    DONE;
+
+}
