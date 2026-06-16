465c91b2a984542ff9a0cbb6009a25b9858a93cf
Lucas Oliveira <lucasp.oliveira822@gmail.com>
2026-06-16 17:52:11 -0300

    feature: add task feature created

--- src/main/java/org/educational/Main.java
+++ src/main/java/org/educational/Main.java
@@ -13,12 +13,41 @@ public class Main {
             System.out.print(PURPLE + "tarefator-cli " + PURPLE);
             command = input.nextLine();
 
-            switch (command) {
-                case "exit": { exitCondition = true; break;}
-                case "help": { System.out.print("\ncommands:\n>help: show all commands avaliable\n>exit: exit application\n\n"); break;}
-                default: { System.out.println("command not allowed, please type 'help' to further informations."); }
-            }
+                String[] processCommand = command.split(" ");
+             if (command.split(" ").length > 3){
+                 System.out.println("maximum of commands inputed. please try again.");
+             } else {
+                 switch (processCommand[0]) {
+                     case "add": {
+                         Task newTask = new Task(processCommand[1]);
+                         System.out.println("task created: \"" + newTask.getDescription() + "\"");
+                     }
+                     // não está finalizado
 
+                     //deleção de task
+
+                     //edição de task
+
+                     //marcação de task
+
+                     //listagem de task
+
+                     case "exit": {
+                         exitCondition = true;
+                         break;
+                     }
+
+                     case "help": {
+                         System.out.print("\ncommands:\n>help: show all commands avaliable\n>exit: exit application\n\n");
+                         break;
+                     }
+
+                     default: {
+                         System.out.println("command not allowed, please type 'help' to further informations.");
+                     }
+                 }
+
+             }
         }
     }
 }
