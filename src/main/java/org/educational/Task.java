45619e5115d3db2af7e358aa4b920a15016de0e3
Lucas Oliveira <lucasp.oliveira822@gmail.com>
2026-06-16 17:51:32 -0300

    feature: getters setters constructors added

--- src/main/java/org/educational/Task.java
+++ src/main/java/org/educational/Task.java
@@ -3,10 +3,61 @@ package org.educational;
 import java.time.LocalDateTime;
 
 public class Task {
-    int id;
-    String description;
-    TaskStatus status;
-    LocalDateTime createdAt;
-    LocalDateTime updatedAt;
+    private int id;
+    private String description;
+    private TaskStatus status;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public TaskStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(TaskStatus status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public Task() {
+    }
+
+
+    public Task(String description){
+        this.id = 1;
+        this.description= description;
+        this.status =TaskStatus.TODO;
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = createdAt;
+    }
 }
