583a47bb1741991058d31673739276e5cdb20f15
Lucas Oliveira <lucasp.oliveira822@gmail.com>
2026-06-18 09:25:23 -0300

    feature: creating JsonFileHandler

---
+++ src/main/java/org/educational/JsonFileHandler.java
@@ -0,0 +1,23 @@
+package org.educational;
+
+public class JsonFileHandler {
+
+    public static final String fileName = "tasks.json";
+
+
+    public JsonFileHandler(){
+        verifyCurrentFile();
+    }
+
+    private void verifyCurrentFile(){
+
+    }
+
+    private void readJsonFile(){
+
+    }
+
+    private void writeJsonFile(){
+
+    }
+}
