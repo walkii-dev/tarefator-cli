a2809c66dacd789234e32303c4f3e00d551208f1
Lucas Oliveira <lucasp.oliveira822@gmail.com>
2026-06-19 14:29:42 -0300

    refactor: organize main code

--- src/main/java/org/educational/Main.java
+++ src/main/java/org/educational/Main.java
@@ -1,37 +1,30 @@
 package org.educational;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
+
 import java.util.Scanner;
 
 public class Main {
 
-    public static Scanner input = new Scanner(System.in);
+    public static final Scanner INPUT = new Scanner(System.in);
 
     public static final String PURPLE = "\u001B[35m";
     public static final String RESET = "\u001B[0m";
 
-    public static final Path FILE_NAME = Path.of("tasks.json");
-
     public static void main(String[] args) {
-
 
         boolean exitCondition = false;
         String command;
 
         while(!exitCondition) {
             System.out.print(PURPLE + "tarefator-cli "+RESET);
-            command = input.nextLine();
+
+            command = INPUT.nextLine();
 
                 String[] processCommand = command.split(" ");
-
                  switch (processCommand[0]) {
                      case "add": {
                          Task.createTask(command.substring(4));
                          break;
                      }
-                     // não está finalizado
 
                      //deleção de task
 
